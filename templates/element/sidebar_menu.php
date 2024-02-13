9aa3bf6278f51d539cac519a3f24c8ee6db531a3
latheefp <latheefp@gmail.com>
2024-02-13 06:33:26 +0300

    adding auto admin forwrad pending mssage

--- templates/element/sidebar_menu.php
+++ templates/element/sidebar_menu.php
@@ -101,7 +101,7 @@
     </a>
     <ul class="nav nav-treeview">
         <li class="nav-item">
-            <a href="/invoices/bills" class="nav-link">
+            <a href="/invoices/" class="nav-link">
                 <i class="fas fa-file-invoice-dollar nav-icon"></i>
                 <p>Invoices</p>
             </a>
